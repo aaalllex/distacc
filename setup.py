f149f6438f8ce90d56d9191e558d3a3552c59c36
Nicco Kunzmann <niccokunzmann@googlemail.com>
2013-04-13 12:15:23 +0200

    added copy notice to setup.p

--- setup.py
+++ setup.py
@@ -13,6 +13,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+# copied from https://github.com/bear/python-twitter/blob/master/setup.py
+#
 
 '''The setup and build script for the python-twitter library.'''
 
